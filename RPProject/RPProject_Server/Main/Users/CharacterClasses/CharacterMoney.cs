308aa8599297acd48eefe284bcd6bfbf35b4fa17
Trinket Entertainment <damienostler1@outlook.com>
2018-05-05 21:12:33 -0400

    Added two functions for managing money
    AddMoney
    RemoveMoney

--- RPProject/RPProject_Server/Main/Users/CharacterClasses/CharacterMoney.cs
+++ RPProject/RPProject_Server/Main/Users/CharacterClasses/CharacterMoney.cs
@@ -1,5 +1,8 @@
 ﻿namespace roleplay.Main.Users.CharacterClasses
 {
+
+    public enum  MoneyTypes { Cash, Bank, Untaxed }
+
     public class CharacterMoney
     {
 
@@ -7,6 +10,41 @@
         public int Cash;
         public int Bank;
         public int UnTaxed;
+
+        public void AddMoney(MoneyTypes type, int amount)
+        {
+            switch (type)
+            {
+                case MoneyTypes.Cash:
+                    Cash += amount;
+                    break;
+                case MoneyTypes.Bank:
+                    Bank += amount;
+                    break;
+                case MoneyTypes.Untaxed:
+                    UnTaxed += amount;
+                    break;
+            }
+        }
+
+        public void RemoveMoney(MoneyTypes type, int amount)
+        {
+            switch (type)
+            {
+                case MoneyTypes.Cash:
+                    if (Cash - amount > 0)
+                        Cash += amount;
+                    break;
+                case MoneyTypes.Bank:
+                    if (Bank - amount > 0)
+                        Bank += amount;
+                    break;
+                case MoneyTypes.Untaxed:
+                    if (UnTaxed - amount > 0)
+                        UnTaxed += amount;
+                    break;
+            }
+        }
 
     }
 }
