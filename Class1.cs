96ae968530c06c8dce34d37e7b7c0ffbd553277e
Genyatta <39069191+Genyatta@users.noreply.github.com>
2018-05-09 17:16:33 -0700

    Update Class1.cs

--- Class1.cs
+++ Class1.cs
@@ -1,150 +1,121 @@
-﻿using System;
-
-
-{
-
-    public sealed class CommandManager
-    {
-        #region Static Features 
-
-        #region Public Events 
-
-   =
-
+InputBindingCollection localInputBindings = null;
+            if (isUIElement)
+            { 
+                localInputBindings = ((UIElement) targetElement).InputBindingsInternal;
+            } 
+            else if (isContentElement) 
+            {
+                localInputBindings = ((ContentElement) targetElement).InputBindingsInternal; 
+            }
+            else if (isUIElement3D)
+            {
+                localInputBindings = ((UIElement3D) targetElement).InputBindingsInternal; 
+            }
+            if (localInputBindings != null) 
+            { 
+                InputBinding inputBinding = localInputBindings.FindMatch(targetElement, inputEventArgs);
+                if (inputBinding != null) 
+                {
+                    command = inputBinding.Command;
+                    target = inputBinding.CommandTarget;
+                    parameter = inputBinding.CommandParameter; 
+                }
+            } 
+  
+            if (command == null) 
+            {
+                lock (_classInputBindings.SyncRoot)
+                {
+                    Type classType = targetElement.GetType(); 
+                    while (classType != null)
+                    { 
+                        InputBindingCollection classInputBindings = _classInputBindings[classType] as InputBindingCollection; 
+                        if (classInputBindings != null)
+                        { 
+                            InputBinding inputBinding = classInputBindings.FindMatch(targetElement, inputEventArgs);
+                            if (inputBinding != null)
+                            {
+                                command = inputBinding.Command; 
+                                target = inputBinding.CommandTarget;
+                                parameter = inputBinding.CommandParameter; 
+                                break; 
+                            }
+                        } 
+                        classType = classType.BaseType;
+                    }
+                }
+            } 
  
-        public static void AddPreviewExecutedHandler(UIElement element, ExecutedRoutedEventHandler handler)
-        {
-            if (element == null)
+ 
+            if (command == null) 
             {
-                throw new ArgumentNullException("element");
-            }
-            if (handler == null)
-            {
-                throw new ArgumentNullException("handler");
-            }
-
-            element.AddHandler(PreviewExecutedEvent, handler);
-        }
-
-      
-        public static void RemovePreviewExecutedHandler(UIElement element, ExecutedRoutedEventHandler handler)
-        {
-            if (element == null)
-            {
-                throw new ArgumentNullException("element");
-            }
-            if (handler == null)
-            {
-                throw new ArgumentNullException("handler");
-            }
-
-            element.RemoveHandler(PreviewExecutedEvent, handler);
-        }
-
-        public static readonly RoutedEvent ExecutedEvent =
-                EventManager.RegisterRoutedEvent("Executed",
-                                                 RoutingStrategy.Bubble,
-                                                 typeof(ExecutedRoutedEventHandler),
-                                                 typeof(CommandManager));
-
-
-        public static void AddExecutedHandler(UIElement element, ExecutedRoutedEventHandler handler)
-        {
-            if (element == null)
-            {
-                throw new ArgumentNullException("element");
-            }
-            if (handler == null)
-            {
-                throw new ArgumentNullException("handler");
-            }
-
-            element.AddHandler(ExecutedEvent, handler);
-        }
-
-
-        public static void RemoveExecutedHandler(UIElement element, ExecutedRoutedEventHandler handler)
-        {
-            if (element == null)
-            {
-                throw new ArgumentNullException("element");
-            }
-            if (handler == null)
-            {
-                throw new ArgumentNullException("handler");
-            }
-
-            element.RemoveHandler(ExecutedEvent, handler);
-        }
-
-        public static readonly RoutedEvent PreviewCanExecuteEvent =
-                EventManager.RegisterRoutedEvent("PreviewCanExecute",
-                                                 RoutingStrategy.Tunnel,
-                                                 typeof(CanExecuteRoutedEventHandler),
-                                                 typeof(CommandManager));
-
        
-        public static void AddPreviewCanExecuteHandler(UIElement element, CanExecuteRoutedEventHandler handler)
-        {
-            if (element == null)
+                CommandBindingCollection localCommandBindings = null;
+                if (isUIElement)
+                {
+                    localCommandBindings = ((UIElement) targetElement).CommandBindingsInternal; 
+                }
+                else if (isContentElement) 
+                { 
+                    localCommandBindings = ((ContentElement) targetElement).CommandBindingsInternal;
+                } 
+                else if (isUIElement3D)
+                {
+                    localCommandBindings = ((UIElement3D) targetElement).CommandBindingsInternal;
+                } 
+                if (localCommandBindings != null)
+                { 
+                    command = localCommandBindings.FindMatch(targetElement, inputEventArgs); 
+                }
+            } 
+ 
+            if (command == null)
+            { 
+                lock (_classCommandBindings.SyncRoot)
+                { 
+                    Type classType = targetElement.GetType(); 
+                    while (classType != null)
+                    { 
+                        CommandBindingCollection classCommandBindings = _classCommandBindings[classType] as CommandBindingCollection;
+                        if (classCommandBindings != null)
+                        {
+                            command = classCommandBindings.FindMatch(targetElement, inputEventArgs); 
+                            if (command != null)
+                            { 
+                                break; 
+                            }
+                        } 
+                        classType = classType.BaseType;
+                    }
+                }
+            } 
+ 
+         
+            if (command != null && command != ApplicationCommands.NotACommand) 
             {
-                throw new ArgumentNullException("element");
-            }
-            if (handler == null)
-            {
-                throw new ArgumentNullException("handler");
-            }
+           
+                if (target == null)
+                { 
+                    target = targetElement; 
+                }
+  
+                bool continueRouting = false;
 
-            element.AddHandler(PreviewCanExecuteEvent, handler);
-        }
-
-        public static void RemovePreviewCanExecuteHandler(UIElement element, CanExecuteRoutedEventHandler handler)
-        {
-            if (element == null)
-            {
-                throw new ArgumentNullException("element");
-            }
-            if (handler == null)
-            {
-                throw new ArgumentNullException("handler");
-            }
-
-            element.RemoveHandler(PreviewCanExecuteEvent, handler);
-        }
-
-   
-        public static readonly RoutedEvent CanExecuteEvent =
-                EventManager.RegisterRoutedEvent("CanExecute",
-                                                 RoutingStrategy.Bubble,
-                                                 typeof(CanExecuteRoutedEventHandler),
-                                                 typeof(CommandManager));
-        
-        public static void AddCanExecuteHandler(UIElement element, CanExecuteRoutedEventHandler handler)
-        {
-            if (element == null)
-            {
-                throw new ArgumentNullException("element");
-            }
-            if (handler == null)
-            {
-                throw new ArgumentNullException("handler");
-            }
-
-            element.AddHandler(CanExecuteEvent, handler);
-        }
-
-        public static void RemoveCanExecuteHandler(UIElement element, CanExecuteRoutedEventHandler handler)
-        {
-            if (element == null)
-            {
-                throw new ArgumentNullException("element");
-            }
-            if (handler == null)
-            {
-                throw new ArgumentNullException("handler");
-            }
-
-            element.RemoveHandler(CanExecuteEvent, handler);
-        }
-
-   
+RoutedCommand routedCommand = command as RoutedCommand;
+                if (routedCommand != null) 
+                {
+                    if (routedCommand.CriticalCanExecute(parameter,
+                                                    target,
+                                                    inputEventArgs.UserInitiated ,
+                                                    out continueRouting)) 
+                    {
+                        ExecuteCommand(routedCommand, parameter, target, inputEventArgs);
+                    }
+                } 
+                else
+                { 
+                    if (command.CanExecute(parameter)) 
+                    {
+                        command.Execute(parameter); 
+                    }
+                }
