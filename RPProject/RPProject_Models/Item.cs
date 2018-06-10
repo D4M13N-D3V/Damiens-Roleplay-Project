3d7c63749d67ba8bcd553e402e1dd099da836a60
egertaia <aiaegert@gmail.com>
2018-06-10 16:58:18 +0300

    Removed unused project

--- RPProject/RPProject_Models/Item.cs
+++ RPProject/RPProject_Models/Item.cs
@@ -2,12 +2,12 @@
 {
     public class Item
     {
-        public int Id = 1;
-        public string Name = "Test Item";
-        public string Description = "This is a item for testing. LUL. NOOB.";
-        public int BuyPrice = 100;
-        public int SellPrice = 100;
-        public int Weight = 10;
-        public bool Illegal = false;
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int BuyPrice { get; set; }
+        public int SellPrice { get; set; }
+        public int Weight { get; set; }
+        public bool Illegal { get; set; }
     }
 }
